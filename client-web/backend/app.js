721ad9d97976506f9507801b441ffa7ff297deea
codeguru827 <denn522.guru@gmail.com>
2024-09-16 05:00:04 +0000

    config docker compose and docker setup

--- client-web/backend/app.js
+++ client-web/backend/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  console.log(`Here!`);
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from Backend!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
